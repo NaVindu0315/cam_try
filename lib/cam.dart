bafa71f0522d0eb5f30697df4a3321c4eba17bd9
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2023-11-21 20:51:32 +0530

    second attempt

--- lib/cam.dart
+++ lib/cam.dart
@@ -1,94 +1,73 @@
-import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 import 'dart:io';
 
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
-
-void main() async {
-  // Fetch the available cameras.
-  final cameras = await availableCameras();
-
-  // Get the front camera.
-  final frontCamera = cameras.firstWhere(
-    (camera) => camera.lensFacing == CameraLensFacing.front,
-  );
-
-  // Create a CameraController.
-  final controller = CameraController(
-    frontCamera,
-    ResolutionPreset.medium,
-  );
-
-  // Initialize the controller. This returns a Future.
-  await controller.initialize();
-
-  // Create a directory to store the captured images.
-  final directory = await getApplicationDocumentsDirectory();
-  final imageDirectory = Directory('${directory.path}/captured_images');
-
-  // Check if the directory exists and create it if it doesn't exist.
-  if (!await imageDirectory.exists()) {
-    await imageDirectory.create();
-  }
-
-  // Build the widget tree.
-  runApp(MyApp(
-    controller: controller,
-    imageDirectory: imageDirectory,
-  ));
+List<CameraDescription> cameras = [];
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final CameraController controller;
-  final Directory imageDirectory;
-
-  const MyApp({
-    Key? key,
-    required this.controller,
-    required this.imageDirectory,
-  }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Camera Example',
-      home: TakePictureScreen(
-        controller: controller,
-        imageDirectory: imageDirectory,
-      ),
+      home: CameraScreen(),
     );
   }
 }
 
-class TakePictureScreen extends StatefulWidget {
-  final CameraController controller;
-  final Directory imageDirectory;
-
-  const TakePictureScreen({
-    Key? key,
-    required this.controller,
-    required this.imageDirectory,
-  }) : super(key: key);
-
+class CameraScreen extends StatefulWidget {
   @override
-  State<TakePictureScreen> createState() => _TakePictureScreenState();
+  _CameraScreenState createState() => _CameraScreenState();
 }
 
-class _TakePictureScreenState extends State<TakePictureScreen> {
-  File? imageFile;
-  Future<void>? takePictureFuture;
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      XFile picture = await controller.takePicture();
+      // Save the picture to device storage or perform facial recognition
+      // Here, you could use the 'picture' variable to process the image.
+    } catch (e) {
+      print("Error taking picture: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Hide the camera preview
-    return Container();
-
-    // Take a picture secretly in the background
-    takePictureFuture = widget.controller.takePicture().then((image) {
-      final imageName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
-      final imageFile = File('${widget.imageDirectory.path}/$imageName');
-      image.saveTo(imageFile);
-    });
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera'),
+      ),
+      body: CameraPreview(controller),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: Icon(Icons.camera),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+/*import "package:flutter/material.dart";
 import "package:test1/cam.dart";
 import "cam.dart";
 
@@ -13,6 +13,79 @@ class home extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: cam(),
+    );
+  }
+}*/
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:io';
+
+List<CameraDescription> cameras = [];
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: CameraScreen(),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = CameraController(cameras[0], ResolutionPreset.medium);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _takePicture() async {
+    try {
+      XFile picture = await controller.takePicture();
+      // Save the picture to device storage or perform facial recognition
+      // Here, you could use the 'picture' variable to process the image.
+    } catch (e) {
+      print("Error taking picture: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Camera'),
+      ),
+      body: CameraPreview(controller),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _takePicture,
+        child: Icon(Icons.camera),
+      ),
     );
   }
 }
